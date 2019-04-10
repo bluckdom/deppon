d47ce1a7e316ce10314a01e8c8b72ecb5f6ab6d0
276525649@qq.com <276525649@qq.com>
2019-04-10 17:57:57 +0800

    修改

--- src/components/addOrder.vue
+++ src/components/addOrder.vue
@@ -1,7 +1,34 @@
 <template>
-  <div class="hello">
-   sdf{{msg}}
-    <a-button type="primary">hello world</a-button>
+  <div class="container">
+    <h3 class="tc mt20 toptitle">德邦快递下单处理</h3>
+    <div class="depponbox mt20 oh">
+      <a-row>
+        <a-col :xs="24" :lg="8">
+          <div class="fieldname">
+            <span>*</span>货物名称
+          </div>
+          <div class="inputbox">
+            <a-input :value="cargoName" placeholder="货物名称"></a-input>
+          </div>
+        </a-col>
+        <a-col :xs="24" :lg="8">
+          <div class="fieldname">
+            <span>*</span>支付方式
+          </div>
+          <div class="inputbox">
+            <a-input :value="payType" placeholder="支付方式"></a-input>
+          </div>
+        </a-col>
+        <a-col :xs="24" :lg="8">
+          <div class="fieldname">
+            <span>*</span>运输方式
+          </div>
+          <div class="inputbox">
+            <a-input :value="transportType" placeholder="运输方式"></a-input>
+          </div>
+        </a-col>
+      </a-row>
+    </div>
   </div>
 </template>
 
@@ -10,12 +37,35 @@ export default {
   name: 'addOrder',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      cargoName: ''
     }
+  },
+  computed: {
+  },
+  methods: {
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style>
+  .toptitle{font-size: 16px}
+  .depponbox{
+    background-color: #f7f7f7;
+  border:1px solid #ddd;
+  border-radius: 3px;padding: 20px}
+  .fieldname span{
+    color: #f00;
+  }
+  .fieldname{
+    text-align: right;
+    color: #666;
+    font-size: 14px;
+    width: 80px;
+    font-weight: normal;
+    float: left;
+    line-height: 32px;
+  }
+  .inputbox{
+    margin-left: 100px;}
 </style>
